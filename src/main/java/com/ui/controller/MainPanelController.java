a5e1ba21b4fa7df021b78fbdf3ae5a88060f35dd
HoriaSav <155181609+HoriaSav@users.noreply.github.com>
2025-05-22 22:23:13 +0200

    <fix> UI: border added to menu bar
    <feat> UI: top bar style added

--- src/main/java/com/ui/controller/MainPanelController.java
+++ src/main/java/com/ui/controller/MainPanelController.java
@@ -1,12 +1,8 @@
 package com.ui.controller;
 
 import com.ui.tools.FxmlFileOpener;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 public class MainPanelController {
 
@@ -19,22 +15,5 @@ public class MainPanelController {
         accessController = AccessController.getInstance();
         accessController.setTrackPlayerHelper();
         FxmlFileOpener.loadFrame(stackPane, "album_panel.fxml");
-    }
-
-    @FXML
-    private void minimizeWindow(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setIconified(true);
-    }
-
-    @FXML
-    private void closeApp() {
-        Platform.exit();
-    }
-
-    @FXML
-    private void toggleMaximize(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setMaximized(!stage.isMaximized());
     }
 }
---
+++ src/main/java/com/ui/controller/TopBarController.java
@@ -0,0 +1,29 @@
+package com.ui.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class TopBarController {
+    public void initialize() {
+    }
+
+    @FXML
+    private void minimizeWindow(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void closeApp() {
+        Platform.exit();
+    }
+
+    @FXML
+    private void toggleMaximize(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setMaximized(!stage.isMaximized());
+    }
+}
